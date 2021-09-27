522e49f913e6b760e1c10aea3b1e7beb724e6408
rkb <rumbletumjum@gmail.com>
2021-09-27 16:27:42 -0400

    rc.lua: some housekeeping

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -69,21 +69,8 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.tile,
-    awful.layout.suit.floating,
     awful.layout.suit.max,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
-    awful.layout.suit.corner.nw,
-    -- awful.layout.suit.corner.ne,
-    -- awful.layout.suit.corner.sw,
-    -- awful.layout.suit.corner.se,
+    awful.layout.suit.floating,
 }
 -- }}}
 
@@ -209,7 +196,6 @@ awful.screen.connect_for_each_screen(function(s)
                     {
                         id = 'icon_role',
                         widget = wibox.widget.imagebox,
-                        left = dpi(4),
                     },
                 },
                 {
@@ -244,7 +230,6 @@ awful.screen.connect_for_each_screen(function(s)
                     {
                         id = 'icon_role',
                         widget = wibox.widget.imagebox,
-                        left = dpi(4),
                     },
                 },
                 {
