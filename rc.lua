c501ec14cb0b864f7a9852eb5b5d950a3ecbe4bc
rkb <rumbletumjum@gmail.com>
2021-10-18 01:33:51 -0400

    clean up rules

--- rc.lua
+++ rc.lua
@@ -539,27 +539,8 @@ ruled.client.connect_signal("request::rules", function()
          screen = 1,
          tag    = 'two',
       },
-   },
+   }
 end)
-
--- awful.rules.rules = {
---    {
---       rule = { },
---       properties = {
---          border_width = beautiful.border_width,
---          border_color = beautiful.border_normal,
---          focus = awful.client.focus.filter,
---          raise = true,
---          keys = clientkeys,
---          buttons = clientbuttons,
---          screen = awful.screen.preferred,
---          titlebars_enabled = false,
---          placement = awful.placement.no_overlap+awful.placement.no_offscreen
---       }
---    },
-
---    -- Floating clients.
--- }
 -- }}}
 
 -- {{{ Signals
