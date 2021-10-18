2044f0523136e56a8a2c135ffa473e562696f62d
rkb <rumbletumjum@gmail.com>
2021-10-17 23:22:34 -0400

    remove wibox launcher widget

--- rc.lua
+++ rc.lua
@@ -248,7 +248,6 @@ awful.screen.connect_for_each_screen(function(s)
        layout = wibox.layout.align.horizontal,
        {
           layout = wibox.layout.fixed.horizontal,
-          mylauncher,
           s.mytaglist,
           s.mypromptbox,
        },
