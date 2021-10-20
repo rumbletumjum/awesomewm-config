11518abf5b6d169d20fc65e9ca235f372733e396
rkb <rumbletumjum@gmail.com>
2021-10-20 14:19:40 -0400

    change attach behavior for web tag

--- rc.lua
+++ rc.lua
@@ -617,16 +617,21 @@ end)
 -- {{{ Signals
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function (c)
-    -- Set the windows at the slave,
-    -- i.e. put it at the end of others instead of setting it master.
-    if not awesome.startup then awful.client.setslave(c) end
+   -- Set the windows at the slave,
+   -- i.e. put it at the end of others instead of setting it master.
+   -- if not awesome.startup then awful.client.setslave(c) end
 
-    if awesome.startup
+   local t = awful.screen.focused().selected_tag
+   if not awesome.startup 
+      and t.name ~= "two" then
+      awful.client.setslave(c)
+   end
+   if awesome.startup
       and not c.size_hints.user_position
       and not c.size_hints.program_position then
-        -- Prevent clients from being unreachable after screen count changes.
-        awful.placement.no_offscreen(c)
-    end
+      -- Prevent clients from being unreachable after screen count changes.
+      awful.placement.no_offscreen(c)
+   end
 end)
 
 function double_click_event_handler(double_click_event)
