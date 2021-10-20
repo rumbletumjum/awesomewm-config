318052ad26437cf8d9b9638f0c3a1fdcc157db01
rkb <rumbletumjum@gmail.com>
2021-10-20 14:23:19 -0400

    signals

--- rc.lua
+++ rc.lua
@@ -635,67 +635,67 @@ client.connect_signal("manage", function (c)
 end)
 
 function double_click_event_handler(double_click_event)
-  if double_click_timer then
-    double_click_timer:stop()
-    double_click_timer = nil
-    return true
-  end
+   if double_click_timer then
+      double_click_timer:stop()
+      double_click_timer = nil
+      return true
+   end
 
-  double_click_timer = gears.timer.start_new(0.20, function()
-    double_click_timer = nil
-    return false
-  end)
+   double_click_timer = gears.timer.start_new(0.20, function()
+      double_click_timer = nil
+      return false
+   end)
 end
 -- Add a titlebar if titlebars_enabled is set to true in the rules.
 client.connect_signal("request::titlebars", function(c)
-    -- buttons for the titlebar
-    local buttons = gears.table.join(
-        awful.button({ }, 1, function()
+   -- buttons for the titlebar
+   local buttons = gears.table.join(
+      awful.button({ }, 1, function()
          if double_click_event_handler() then
             c.maximized = not c.maximized
          else
             c:emit_signal("request::activate", "titlebar", {raise = true})
             awful.mouse.client.move(c)
          end
-        end),
-        awful.button({ }, 3, function()
-            c:emit_signal("request::activate", "titlebar", {raise = true})
-            awful.mouse.client.resize(c)
-        end)
-    )
+      end),
+      awful.button({ }, 3, function()
+         c:emit_signal("request::activate", "titlebar", {raise = true})
+         awful.mouse.client.resize(c)
+      end)
+   )
 
-    awful.titlebar(c) : setup {
-        { -- Left
-            awful.titlebar.widget.iconwidget(c),
-            buttons = buttons,
-            layout  = wibox.layout.fixed.horizontal
-        },
-        { -- Middle
-            { -- Title
-               font = beautiful.font_title,
-                align  = "center",
-                widget = awful.titlebar.widget.titlewidget(c)
-            },
-            buttons = buttons,
-            layout  = wibox.layout.flex.horizontal
-        },
-        { -- Right
-            -- awful.titlebar.widget.floatingbutton (c),
-            -- awful.titlebar.widget.stickybutton   (c),
-            -- awful.titlebar.widget.ontopbutton    (c),
-            awful.titlebar.widget.minimizebutton(c),
-            awful.titlebar.widget.maximizedbutton(c),
-            awful.titlebar.widget.closebutton    (c),
-            layout = wibox.layout.fixed.horizontal()
-        },
-        layout = wibox.layout.align.horizontal
-    }
+   awful.titlebar(c) : setup {
+      { -- Left
+         awful.titlebar.widget.iconwidget(c),
+         buttons = buttons,
+         layout  = wibox.layout.fixed.horizontal
+      },
+      { -- Middle
+         { -- Title
+            font = beautiful.font_title,
+            align  = "center",
+            widget = awful.titlebar.widget.titlewidget(c)
+         },
+         buttons = buttons,
+         layout  = wibox.layout.flex.horizontal
+      },
+      { -- Right
+         -- awful.titlebar.widget.floatingbutton (c),
+         -- awful.titlebar.widget.stickybutton   (c),
+         -- awful.titlebar.widget.ontopbutton    (c),
+         awful.titlebar.widget.minimizebutton(c),
+         awful.titlebar.widget.maximizedbutton(c),
+         awful.titlebar.widget.closebutton    (c),
+         layout = wibox.layout.fixed.horizontal()
+      },
+      layout = wibox.layout.align.horizontal
+   }
 end)
 
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
-    -- c:emit_signal("request::activate", "mouse_enter", {raise = false})
-    -- c:emit_signal("request::activate", "mouse_enter", {raise = false})
+   -- c:emit_signal("request::activate", "mouse_enter", {raise = false})
+   -- c:emit_signal("request::activate", "mouse_enter", {raise = false})
    c:activate { context = "mouse_enter", { raise = false } }
 end)
 
