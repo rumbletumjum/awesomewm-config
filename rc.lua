ce839e5910b65c6b4aa7f5d497f7fdacd2dfe552
rkb <rumbletumjum@gmail.com>
2021-10-20 14:37:57 -0400

    menus

--- rc.lua
+++ rc.lua
@@ -91,23 +91,24 @@ local menu_awesome = { "awesome", myawesomemenu, beautiful.awesome_icon }
 local menu_terminal = { "open terminal", terminal }
 
 if has_fdo then
-    mymainmenu = freedesktop.menu.build({
-        before = { menu_awesome },
-        after =  { menu_terminal }
-    })
+   mymainmenu = freedesktop.menu.build({
+      before = { menu_awesome },
+      after =  { menu_terminal }
+   })
 else
-    mymainmenu = awful.menu({
-        items = {
-                  menu_awesome,
-                  { "Debian", debian.menu.Debian_menu.Debian },
-                  menu_terminal,
-                }
-    })
+   mymainmenu = awful.menu({
+      items = {
+         menu_awesome,
+         { "Debian", debian.menu.Debian_menu.Debian },
+         menu_terminal,
+      }
+   })
 end
 
-
-mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
-                                     menu = mymainmenu })
+mylauncher = awful.widget.launcher({
+   image = beautiful.awesome_icon,
+   menu = mymainmenu
+})
 
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
